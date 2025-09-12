43b7d2a635621d31a6de239e71a3e9aa07f8fa0f
Pui Hei Lam <puihei@student.unimelb.edu.au>
2025-09-12 18:39:44 +1000

    FIX: responsive quick close button location

--- client/src/QuickClose.tsx
+++ client/src/QuickClose.tsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 function QuickClose () {
     return (
-        <div className="fixed top-0 left-0 w-full group justify-center">
-            <div className="flex justify-end px-20 py-2 bg-[#DCEAAB] opacity-100 -translate-y-full transition-all duration-150 ease-in-out pointer-events-none group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto">
-                <Link to="/register" className="flex items-center mx-2 px-10 py-2 text-lg font-semibold text-[#01563E] no-underline bg-[#FDB4C6] rounded-3xl">Quick Close</Link>
-            </div>
-        </div>
+        <Link
+            to="/register"
+            className="flex items-center justify-center mx-2 px-4 sm:px-10 py-2 text-md sm:text-lg font-semibold text-[#01563E] no-underline bg-[#FDB4C6] rounded-3xl hover:bg-[#FFDBE4]"
+            >
+                Quick Close
+        </Link>
     )
 }
 
