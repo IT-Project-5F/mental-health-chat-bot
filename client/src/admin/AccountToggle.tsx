d69fea4d41760a21b7901b5cf0fb2b7167b315cc
Pui Hei Lam <puihei@student.unimelb.edu.au>
2025-09-17 11:25:17 +1000

    FIX: avatar image type

--- client/src/admin/AccountToggle.tsx
+++ client/src/admin/AccountToggle.tsx
@@ -1,9 +1,14 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 function AccountToggle() {
     return (
         <div className="mx-4 mt-2 px-4 py-6 border-b-1">
-            <div className="flex items-center space-x-6">
-                <h1 className="rounded-full bg-white text-gray-500 w-20 h-20 flex items-center justify-center">Avatar</h1>
-                <div className="text-left overflow-x-hidden">
+            <div className="flex items-center space-x-8">
+                <Avatar className="size-12 sm:size-16">
+                    <AvatarImage src="https://github.com/shadcn.png"/>
+                    <AvatarFallback>JD</AvatarFallback>
+                </Avatar>
+                <div className="text-left text-white overflow-x-hidden hidden sm:block">
                     <h1 className="text-xl font-bold">John Doe</h1>
                     <p className="text-sm">admin</p>
                     <p className="text-sm break-words max-w-[180px]">very_very_long_email_of_johndoe@health.vic.org.au</p>
