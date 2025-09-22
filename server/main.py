95ab3ffd86ad7a14578cc43f53884ca40ca71e5b
Abraham Ming Moncherry <65388998+Abraham-Moncherry@users.noreply.github.com>
2025-09-23 00:49:44 +1000

    Database (#8)
    * Remove tracked __pycache__ files
    
    * CREATE: CRUD database endpoints

--- server/main.py
+++ server/main.py
@@ -4,15 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from auth.Routes  import router as auth_router
 from users.Routes import router as users_router
 from chat.Routes  import router as chat_router
+from database.Routes import router as database_router
 from auth.Database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Mental Health Chat Bot API", version="1.0.0")
 
-app.include_router(auth_router, prefix  = '/api/auth', tags  = ["auth"]) 
+app.include_router(auth_router, prefix  = '/api/auth', tags  = ["auth"])
 app.include_router(users_router, prefix = '/api/users', tags = ["users"])
 app.include_router(chat_router,  prefix = '/api/chat', tags  = ["chat"])
+app.include_router(database_router, prefix = '/api/database', tags = ["database"])
 
 # Configure CORS
 app.add_middleware(
