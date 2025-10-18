b9b3b19e3d6235153c3e14136eb679457ce4774d
Pui Hei Lam <puihei@student.unimelb.edu.au>
2025-10-18 23:22:18 +1100

    FIX: external redirect

--- client/src/QuickClose.tsx
+++ client/src/QuickClose.tsx
@@ -1,11 +1,9 @@
-import { useNavigate } from "react-router-dom";
 import { Button } from "./components/ui/button";
 
 function QuickClose () {
-    const navigate = useNavigate();
     return (
         <Button
-            onClick={() => navigate("https://www.google.com/")}
+            onClick={() => window.location.href = "https://www.google.com/"}
             variant={"destructive"}
             size={"lg"}
         >
